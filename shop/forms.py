8c8a85e39cebd2685769e2da3353a5762c922dea
vatari <majestic13@gmail.com>
2024-03-09 08:16:33 +0200

    forms partial added

---
+++ shop/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+
+
+class NewOfferForm(forms.Form):
+    CATEGORIES = (
+        (1, "Настолни компютри"),
+        (2, "Лаптопи"),
+        (3, "Сървъри"),
+        (4, "Периферия"),
+    )
+
+    name = forms.CharField()
+    description = forms.CharField()
+    price = forms.DecimalField(decimal_places=2, widget=forms.NumberInput)
+    category = forms.ChoiceField(choices=CATEGORIES, widget=forms.RadioSelect)
+    image = forms.ImageField()
+
+
+class EditOfferForm(forms.Form):
+    CATEGORIES = (
+        (1, "Настолни компютри"),
+        (2, "Лаптопи"),
+        (3, "Сървъри"),
+        (4, "Периферия"),
+    )
+
+    name = forms.CharField()
+    description = forms.CharField()
+    price = forms.DecimalField(decimal_places=2, widget=forms.NumberInput)
+    category = forms.ChoiceField(choices=CATEGORIES, widget=forms.RadioSelect)
+
+    def __init__(self, *args, **kwargs):
+        item = kwargs.pop("item", None)
+        super(EditOfferForm, self).__init__(*args, **kwargs)
+        self.fields["name"].required = False
+        self.fields["description"].required = False
+        self.fields["price"].required = False
+        self.fields["category"].required = False
+
+        if item:
+            self.fields["name"].widget.attrs["placeholder"] = item.name
+            self.fields["description"].widget.attrs["placeholder"] = item.description
+            self.fields["price"].widget.attrs["placeholder"] = item.price
