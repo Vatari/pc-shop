ada2c7fd40778bf00820863b538638c37552c6f8
vatari <majestic13@gmail.com>
2024-04-04 11:12:45 +0300

    added admin custom model

--- shop/accounts/admin.py
+++ shop/accounts/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('username', 'email', 'is_staff')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'repassword')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('username', 'email', 'password1', 'password2'),
+            },
+        ),
+    )
+
+    filter_horizontal = ()
+    list_filter = ('is_active', 'is_staff')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
